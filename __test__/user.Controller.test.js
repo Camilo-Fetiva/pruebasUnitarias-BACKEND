38b71429f587679cf3ec489066fdba3b2df21129
Camilo-Fetiva <kmilofetiva@gmail.com>
2025-01-13 21:26:01 -0500

    [crear] repositorio de pruebas unitarias para el proyecto final

---
+++ __test__/user.Controller.test.js
@@ -0,0 +1,70 @@
+// IMPORTAR DEPENDENCIAS
+import supertest from "supertest";
+import app from "../app.js";
+import mongoose from "mongoose";
+
+import { userModel } from "../src/models/user.model.js";
+
+// BLOQUES DE PRUEBA
+describe(
+    'Pruebas de controladores de los usuarios',
+    ()=>{
+        //Limpiar la base de datos antes de cada prueba 
+        beforeEach(async ()=>{
+            await userModel.deleteMany({});
+        });
+
+        // CERRAR ;a conexion a MongoDB despues de las pruebas
+        afterAll(async ()=>{
+            await mongoose.connection.close();
+        });
+
+        // PRUEBA DE USUARIO CREADO
+        const testUser = {
+            Nombre: 'Juan Roman Riquelme',
+            Correo: ' inventeroman@user.com',
+            Contrasena: '123',
+        }
+
+        // POST
+        describe(
+            'Pruebas POST para usuarios',
+            ()=>{
+                it(
+                    'Creacion de usuarios correcta',
+                    async()=>{
+                        const res = await supertest(app).post('/usuarios').send(testUser)
+
+                    // DEFINIR QUE SE ESPERA DE LA RESPUESTA
+                    expect(res.statusCode).toBe(201);
+                    }
+                )
+
+                it(
+                    'ERROR al faltar informacion',
+                    async()=>{
+                        const res = await supertest(app).post('/usuarios').send({email:testUser.email})
+
+                        // DEFINIR QUE SE ESPERA DE LA RESPUESTA
+                        expect(res.body).toHaveProperty('mensaje', 'Error al crear un usuario');
+                    }
+                )
+            }
+        )
+
+        // GET
+        describe(
+            'Pruebas GET para usuarios',
+            ()=>{
+                it(
+                    'Deberia indicar que no hay usuarios almacenados',
+                    async()=>{
+                        const res = await supertest(app).get('/usuarios');
+                        expect(res.statusCode).toBe(200);
+                        expect(res.body).toHaveProperty('mensaje', 'No hay usuarios en Ecocloset')
+                    }
+                )
+            }
+        )
+    }
+)
---
+++ app.js
@@ -0,0 +1,31 @@
+// 1. Importar las dependencias y módulos necesarios 
+import express from 'express'; //importar express para la prueba en la terminal
+import dotenv from 'dotenv'; //Dependencia para manejar variables de entorno
+import { connectionMongo } from './src/config/dataBase.js'; // dependencia para conectar la base de datos
+import { productRouter } from './src/routes/products.routes.js';
+import { userRouter } from './src/routes/users.routes.js';
+import { orderRouter } from './src/routes/order.routes.js';
+import { loginRouter } from './src/routes/login.routes.js';
+import { adminRouter } from './src/routes/admin.routes.js';
+
+// Dependencia para la conexion con el frontend
+import cors from 'cors';
+
+
+// 2. CONFIGURAR EL USO DEL SERVIDOR
+const app = express();
+dotenv.config();
+const port = process.env.PORT
+app.use(cors()); // <- Uso para utilizar el backend en el navegador
+
+app.use(express.json()); //Usar formato JSON, CREAR y ACTUALIZAR datos
+app.use ( '/productos', productRouter);
+app.use ('/usuarios', userRouter);
+app.use ('/ordenes', orderRouter);
+app.use ('/login', loginRouter);
+app.use ( '/administrador', adminRouter);
+
+//INVOCAR LA FUNCION DE LA BASE DE DATOS
+connectionMongo ();
+
+export default app;
